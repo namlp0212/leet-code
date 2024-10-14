82e9dd6896359f3a985899774d5d0ec7ad175103
namlp0212 <namlp0212@gmail.com>
2024-10-14 15:28:18 +0700

    9_Can Jump

---
+++ src/top_150_interview/i_Jump_game.java
@@ -0,0 +1,83 @@
+package top_150_interview;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class i_Jump_game {
+    static class Node {
+        int idx;
+        int val;
+
+        public Node(int idx, int val) {
+            this.idx = idx;
+            this.val = val;
+        }
+    }
+    public static boolean canJumpV1(int[] nums) {
+        boolean[] check = new boolean[nums.length];
+
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(new Node(0, nums[0]));
+        check[0] = true;
+        boolean canJump = false;
+
+        int val;
+        int idx;
+        while (!queue.isEmpty()) {
+            Node first = queue.poll();
+
+            idx = first.idx;
+            val = first.val;
+
+            if (idx == nums.length - 1) {
+                return true;
+            } else {
+                for (int i = idx + 1; i <= idx + val && i < nums.length; i++) {
+                    if (!check[i]) {
+                        queue.add(new Node(i, nums[i]));
+                        check[i] = true;
+                    }
+                }
+            }
+        }
+
+        return canJump;
+    }
+
+    public static boolean canJumpV2(int[] nums) {
+        int len = nums.length;
+        boolean[] check = new boolean[len];
+
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(0);
+        check[0] = true;
+        boolean canJump = false;
+
+        int val;
+        int idx;
+        while (!queue.isEmpty()) {
+            idx = queue.poll();
+            val = nums[idx];
+
+            if (idx == len - 1) {
+                return true;
+            } else {
+                if (idx + val >= len - 1) return true;
+                int maxJump = 0;
+                for (int i = idx + val; i >= idx + 1; i--) {
+                    if (!check[i] && (i + nums[i] > maxJump)) {
+                        queue.add(i);
+                        check[i] = true;
+                    }
+                }
+            }
+        }
+
+        return canJump;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 3, 1, 1, 4};
+        System.out.println(canJumpV2(nums));
+    }
+}
