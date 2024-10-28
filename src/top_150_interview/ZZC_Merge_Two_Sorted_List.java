621846ef183e3937d500197210f42e3b686e938d
namlp0212 <namlp0212@gmail.com>
2024-10-28 09:05:05 +0700

    50_Merged two sorted lit

---
+++ src/top_150_interview/ZZC_Merge_Two_Sorted_List.java
@@ -0,0 +1,36 @@
+package top_150_interview;
+
+public class ZZC_Merge_Two_Sorted_List {
+    class ListNode {
+          int val;
+          ListNode next;
+          ListNode() {}
+          ListNode(int val) { this.val = val; }
+          ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(-1);
+        ListNode current = dummy;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                current.next = l1;
+                l1 = l1.next;
+            } else {
+                current.next = l2;
+                l2 = l2.next;
+            }
+            current = current.next;
+        }
+
+        // Attach the remaining nodes
+        if (l1 != null) {
+            current.next = l1;
+        } else {
+            current.next = l2;
+        }
+
+        return dummy.next;
+    }
+}
