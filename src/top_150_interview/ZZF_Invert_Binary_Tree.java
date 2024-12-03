8915f85ad86347734cb57812bfe67f53f5b27482
namlp0212 <namlp0212@gmail.com>
2024-12-03 17:44:40 +0700

    53_Revert_Binary_Tree

---
+++ src/top_150_interview/ZZF_Invert_Binary_Tree.java
@@ -0,0 +1,25 @@
+package top_150_interview;
+
+public class ZZF_Invert_Binary_Tree {
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) return null;
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+        root.left = right;
+        root.right = left;
+        return root;
+    }
+}
